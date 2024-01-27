1fb566f27a0790662bd3f4a9c8c101cc2c564ba0
aishwaryavenkatesan <venkatesan.ai@northeastern.edu>
2024-01-27 14:08:27 -0500

    adding code for lowest common ancestor of a binary search tree leetcode medium

---
+++ bfs and dfs/lowestCommonAncestorOfBST.java
@@ -0,0 +1,30 @@
+
+public class lowestCommonAncestorOfBST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+       }
+    }
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if(root == null)
+        return null;
+
+        if(p.val < root.val && q.val < root.val)
+        return lowestCommonAncestor(root.left, p, q);
+
+        if(p.val > root.val && q.val > root.val)
+        return lowestCommonAncestor(root.right, p, q);
+
+        return root;
+        
+    }
+    
+}
