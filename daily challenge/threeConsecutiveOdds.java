f1d5f785bd63d4480526b765b23fcc45b10c68ca
aishwaryavenkatesan <venkatesan.ai@northeastern.edu>
2024-07-01 23:52:04 -0400

    three consecutive odds leetcode easy

---
+++ daily challenge/threeConsecutiveOdds.java
@@ -0,0 +1,25 @@
+public class threeConsecutiveOdds {
+        public boolean threeConsecutiveOddsFunc(int[] arr) {
+            
+            int count = 0;
+    
+            for(int i =0; i < arr.length; i++)
+            {
+                if(arr[i] % 2 != 0)
+                {
+                    count++;
+    
+                    if(count == 3)
+                    return true;
+                }
+                else
+                {
+                    count = 0;
+                }
+                
+            }
+    
+            return false;
+        }
+
+}
