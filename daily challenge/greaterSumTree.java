c2c40da235f8c4375d870ef28018621560d11a64
aishwaryavenkatesan <venkatesan.ai@northeastern.edu>
2024-07-02 23:49:37 -0400

    bst to greater sum tree

---
+++ daily challenge/greaterSumTree.java
@@ -0,0 +1,33 @@
+
+
+public class greaterSumTree {
+
+     public class TreeNode {
+             int val;
+             TreeNode left;
+             TreeNode right;
+             TreeNode() {}
+            TreeNode(int val) { this.val = val; }
+            TreeNode(int val, TreeNode left, TreeNode right) {
+                 this.val = val;
+                 this.left = left;
+                 this.right = right;
+             }
+         }
+
+    int val = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        
+        if(root != null)
+        {
+            bstToGst(root.right);
+            val+= root.val;
+            root.val = val;
+            bstToGst(root.left);
+        }
+
+        return root;
+
+    
+}
+}
