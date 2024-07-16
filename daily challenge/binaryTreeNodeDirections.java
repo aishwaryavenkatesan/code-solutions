aad155263b851764232b831d9ea6fbd95f626812
aishwaryavenkatesan <venkatesan.ai@northeastern.edu>
2024-07-16 00:01:04 -0400

    step by step directions from binary tree node to another

---
+++ daily challenge/binaryTreeNodeDirections.java
@@ -0,0 +1,70 @@
+import javax.swing.tree.TreeNode;
+
+public class binaryTreeNodeDirections {
+
+    public class TreeNode {
+        int val;
+      TreeNode left;
+        TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+    public String getDirections(TreeNode root, int startValue, int destValue) {
+        StringBuilder s = new StringBuilder();
+        StringBuilder d = new StringBuilder();
+        TreeNode ancestor = lca(root, startValue, destValue);
+        pathString(ancestor, startValue, s);
+        pathString(ancestor, destValue, d);
+
+        s.reverse().toString();
+
+
+        return "U".repeat(s.length()) + d.reverse().toString();
+    }
+
+    public TreeNode lca(TreeNode root, int startValue, int destValue)
+    {
+        if(root == null)
+        return null;
+
+
+        if(root.val == startValue || root.val == destValue)
+        return root;
+
+        TreeNode lTree = lca(root.left, startValue, destValue);
+        TreeNode rTree = lca(root.right, startValue, destValue);
+
+        if(lTree != null && rTree != null)
+        return root;
+
+        return (lTree == null) ? rTree : lTree;
+    }
+
+    public boolean pathString(TreeNode root, int startValue, StringBuilder res)
+    {
+        if(root == null)
+        return false;
+
+        if(root.val == startValue)
+        return true;
+
+        boolean left = pathString(root.left, startValue, res);
+        boolean right = pathString(root.right, startValue, res);
+
+        if(left)
+        res.append("L");
+
+        else if(right)
+        res.append("R");
+
+        return left || right;
+
+    }
+
+
+}
